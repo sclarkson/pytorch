e0045d31103cb7f346a972292ae8a864c7d46d8b
Haixin Liu <haixin@fb.com>
2019-09-11 18:04:50 -0700

    [quant][observer] Add l2 norm minimization
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/24022
    
    In histogram observer add an approximation for L2 error minimization for selecting min/max.
    By selecting new min/max, we filter out outliers in input distribution.
    
    This follows the implementation of NormMinimization::NonlinearQuantizationParamsSearch in caffe2/quantization/server/norm_minimization.cc
    ghstack-source-id: 89956490
    
    Differential Revision: [D16713239](https://our.internmc.facebook.com/intern/diff/D16713239/)

--- test/test_quantization.py
+++ test/test_quantization.py
@@ -812,24 +812,16 @@ class QuantizationDebugTest(QuantizationTestCase):
         y = torch.tensor([4.0, 5.0, 5.0, 6.0, 7.0, 8.0])
         myobs(x)
         myobs(y)
-        self.assertEqual(myobs.relaxed_min, -1.5)
-        self.assertEqual(myobs.relaxed_max, 8.5)
-        self.assertEqual(myobs.histogram, [0., 0., 1., 2., 1., 2., 3., 2., 1., 1.])
+        self.assertEqual(myobs.min_val, 1.0)
+        self.assertEqual(myobs.max_val, 8.0)
+        self.assertEqual(myobs.histogram, [1., 2., 0., 1., 2., 2., 2., 1., 1., 1.])
         qparams = myobs.calculate_qparams()
-        if reduce_range:
-            if qscheme == torch.per_tensor_symmetric:
-                ref_scale = 0.066666 * 255 / 127
-                ref_zero_point = 0 if qdtype is torch.qint8 else 128
-            else:
-                ref_scale = 0.0333333 * 255 / 127
-                ref_zero_point = -64 if qdtype is torch.qint8 else 0
+        if qscheme == torch.per_tensor_symmetric:
+            ref_scale = 0.062745
+            ref_zero_point = 0 if qdtype is torch.qint8 else 128
         else:
-            if qscheme == torch.per_tensor_symmetric:
-                ref_scale = 0.066666
-                ref_zero_point = 0 if qdtype is torch.qint8 else 128
-            else:
-                ref_scale = 0.0333333
-                ref_zero_point = -128 if qdtype is torch.qint8 else 0
+            ref_scale = 0.0313725
+            ref_zero_point = -128 if qdtype is torch.qint8 else 0
         self.assertEqual(qparams[1].item(), ref_zero_point)
         self.assertAlmostEqual(qparams[0].item(), ref_scale, delta=1e-5)
 
